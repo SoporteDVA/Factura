17179dee2cb5db8185f002624ec10a944f662bb9
JoseLuisLopezs <jlopez@corpdva.com>
2018-07-19 15:13:28 -0600

    Cambios_Varios Index STX (Jose)

--- www/WSFE/index.php
+++ www/WSFE/index.php
@@ -785,6 +785,65 @@ function signFE($p12Url,$pinP12,$inXml,$tipoDoc) {
 }
 
 
+//FUNCION QUE FIRMA EL MENSAJE RECEPTOR
+
+function signMR($p12Url,$pinP12,$inXml,$tipoDoc) {
+    require 'Firmador/Firmadohaciendacr.php';
+    //modules_loader("files");
+    $p12Url = 'Firmas/060082019712.p12';
+    $pinP12 = '2018';
+    $inXml = $inXml;
+    $tipoDoc = 'CCE';
+    $tipoDocumento;
+    $tipos = array("FE", "ND", "NC", "TE","CCE","CPCE","RCE");
+    if (in_array($tipoDoc, $tipos)) {
+        switch ($tipoDoc) {
+            case 'FE': //Factura Electronica
+                $tipoDocumento = "01";
+                break;
+            case 'ND': // Nota de Debito
+                $tipoDocumento = "02";
+                break;
+            case 'NC': // Nota de Credito
+                $tipoDocumento = "03";
+                break;
+            case 'TE': // Tiquete Electronico
+                $tipoDocumento = "04";
+                break;
+            case 'CCE': // Confirmacion Comprabante Electronico
+                $tipoDocumento = "05";
+                break;
+            case 'CPCE': // Confirmacion Parcial Comprbante Electronico
+                $tipoDocumento = "06";
+                break;
+            case 'RCE': // Rechazo Comprobante Electronico
+                $tipoDocumento = "07";
+                break;
+        }
+    } else {
+        return "No se encuentra tipo de documento";
+    }
+
+
+    $fac = new Firmadocr();
+    //$inXmlUrl debe de ser en Base64 
+    //$p12Url es un downloadcode previamente suministrado al subir el certificado en el modulo fileUploader -> subir_certif
+    //Tipo es el tipo de documento 
+    //01 FE
+    //02 ND
+    //03 NC
+    //04 TE
+    //05 06 07 Mensaje Receptor
+
+    $returnFile = $fac->firmar($p12Url, $pinP12, $inXml, $tipoDocumento);
+    //$arrayResp = array(
+      //  "xmlFirmado" => $returnFile
+    //);
+
+    return $returnFile;
+}
+
+
 
 function EnviaFAC($clave,$fecha,$emi_tipoid,$emi_identificacion,$recp_tipoid, $recp_identificacion, $XMLIN, $token ) {
     
@@ -1281,6 +1340,12 @@ $soapclient->register('genXMLMR', array(
     ), $ns);   
 
 $soapclient->register('signFE', array(
+    'p12Url' => 'xsd:string','pinP12' => 'xsd:string' ,'inXml' => 'xsd:string','tipoDoc' => 'xsd:string'
+), array(
+    'xmlOUT' => 'xsd:string'
+), $ns);
+
+$soapclient->register('signMR', array(
     'p12Url' => 'xsd:string','pinP12' => 'xsd:string' ,'inXml' => 'xsd:string','tipoDoc' => 'xsd:string'
 ), array(
     'xmlOUT' => 'xsd:string'
