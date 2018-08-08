e8b4fc219d502efc146dd492f695af0ad9b2d9e6
DVA <soporte@corpdva.com>
2018-08-08 16:34:59 -0600

    6663333333

--- www/FacturacionDVA/public/scripts/Cliente.js
+++ www/FacturacionDVA/public/scripts/Cliente.js
@@ -137,5 +137,766 @@ function cargarDataCliente(id, tipo_persona,nombre,tipo_documento,num_documento,
  		$("#txtEmail").val(email);
  		$("#txtNumero_Cuenta").val(numero_cuenta);
  		$("#cboEstado").val(estado);
- 	}
+	 }
+	 
+
+// Provincia, Cantón y Distrito
+
+canton = new Array(
+	new Array(
+		new Array("SAN JOSE",1),
+		new Array("ESCAZU",2),
+		new Array("DESAMPARADOS",3),
+		new Array("PURISCAL",4),
+		new Array("TARRAZU",5),
+		new Array("ASERRI",6),
+		new Array("MORA",7),
+		new Array("GOICOECHEA",8),
+		new Array("SANTA ANA",9),
+		new Array("ALAJUELITA",10),
+		new Array("VAZQUEZ DE CORONADO",11),
+		new Array("ACOSTA",12),
+		new Array("TIBAS",13),
+		new Array("MORAVIA",14),
+		new Array("MONTES DE OCA",15),
+		new Array("TURRUBARES",16),
+		new Array("DOTA",17),
+		new Array("CURRIDABAT",18),
+		new Array("PEREZ ZELEDON",19),
+		new Array("LEON CORTES",20)
+	),
+	new Array(
+		new Array("ALAJUELA",1),
+		new Array("SAN RAMON",2),
+		new Array("GRECIA",3),
+		new Array("SAN MATEO",4),
+		new Array("ATENAS",5),
+		new Array("NARANJO",6),
+		new Array("PALMARES",7),
+		new Array("POAS",8),
+		new Array("OROTINA",9),
+		new Array("SAN CARLOS",10),
+		new Array("ALFARO RUIZ",11),
+		new Array("VALVERDE VEGA",12),
+		new Array("UPALA",13),
+		new Array("LOS CHILES",14),
+		new Array("GUATUSO",15)
+	),
+	new Array(
+		new Array("CARTAGO",1),
+		new Array("PARAISO",2),
+		new Array("LA UNION",3),
+		new Array("JIMENEZ",4),
+		new Array("TURRIALBA",5),
+		new Array("ALVARADO",6),
+		new Array("OREAMUNO",7),
+		new Array("EL GUARCO",8)
+	),
+	new Array(
+		new Array("HEREDIA",1),
+		new Array("BARVA",2),
+		new Array("SANTO DOMINGO",3),
+		new Array("SANTA BARBARA",4),
+		new Array("SAN RAFAEL",5),
+		new Array("SAN ISIDRO",6),
+		new Array("BELEN",7),
+		new Array("FLORES",8),
+		new Array("SAN PABLO",9),
+		new Array("SARAPIQUI",10)
+	),
+	new Array(
+		new Array("LIBERIA",1),
+		new Array("NICOYA",2),
+		new Array("SANTA CRUZ",3),
+		new Array("BAGACES",4),
+		new Array("CARRILLO",5),
+		new Array("CAÑAS",6),
+		new Array("ABANGARES",7),
+		new Array("TILARAN",8),
+		new Array("NANDAYURE",9),
+		new Array("LA CRUZ",10),
+		new Array("HOJANCHA",11)
+	),
+	new Array(
+		new Array("PUNTARENAS",1),
+		new Array("ESPARZA",2),
+		new Array("BUENOS AIRES",3),
+		new Array("MONTES DE ORO",4),
+		new Array("OSA",5),
+		new Array("AGUIRRE",6),
+		new Array("GOLFITO",7),
+		new Array("COTO BRUS",8),
+		new Array("PARRITA",9),
+		new Array("CORREDORES",10),
+		new Array("GARABITO",11)
+	),
+	new Array(
+		new Array("LIMON",1),
+		new Array("POCOCI",2),
+		new Array("SIQUIRRES",3),
+		new Array("TALAMANCA",4),
+		new Array("MATINA",5),
+		new Array("GUACIMO",6)
+	)
+	);
+	
+	distrito = new Array(
+	new Array(
+		new Array(
+			new Array("EL CARMEN",1),
+			new Array("MERCED",2),
+			new Array("HOSPITAL",3),
+			new Array("CATEDRAL",4),
+			new Array("ZAPOTE",5),
+			new Array("SAN FRANCISCO DOS RIOS",6),
+			new Array("LA URUCA",7),
+			new Array("MATA REDONDA",8),
+			new Array("PAVAS",9),
+			new Array("HATILLO",10),
+			new Array("SAN SEBASTIAN",11)
+		),
+		new Array(
+			new Array("ESCAZU",1),
+			new Array("SAN ANTONIO",2),
+			new Array("SAN RAFAEL",3)
+		),
+		new Array(
+			new Array("DESAMPARADOS",1),
+			new Array("SAN MIGUEL",2),
+			new Array("SAN JUAN DE DIOS",3),
+			new Array("SAN RAFAEL ARRIBA",4),
+			new Array("SAN ANTONIO",5),
+			new Array("FRAILES",6),
+			new Array("PATARRA",7),
+			new Array("SAN CRISTOBAL",8),
+			new Array("ROSARIO",9),
+			new Array("DAMAS",10),
+			new Array("SAN RAFAEL ABAJO",11),
+			new Array("GRAVILIAS",12)
+		),
+		new Array(
+			new Array("SANTIAGO",1),
+			new Array("MERCEDES SUR",2),
+			new Array("BARBACOAS",3),
+			new Array("GRIFO ALTO",4),
+			new Array("SAN RAFAEL",5),
+			new Array("CANDELARITA",6),
+			new Array("DESAMPARADITOS",7),
+			new Array("SAN ANTONIO",8),
+			new Array("CHIRES",9)
+		),
+		new Array(
+			new Array("SAN MARCOS",1),
+			new Array("SAN LORENZO",2),
+			new Array("SAN CARLOS",3)
+		),
+		new Array(
+			new Array("ASERRI",1),
+			new Array("TARBACA",2),
+			new Array("VUELTA DE JORCO",3),
+			new Array("SAN GABRIEL",4),
+			new Array("LEGUA",5),
+			new Array("MONTERREY",6)
+		),
+		new Array(
+			new Array("CIUDAD COLON",1),
+			new Array("GUAYABO",2),
+			new Array("TABARCIA",3),
+			new Array("PIEDRAS NEGRAS",4),
+			new Array("PICAGRES",5)
+		),
+		new Array(
+			new Array("GUADALUPE",1),
+			new Array("SAN FRANCISCO",2),
+			new Array("CALLE BLANCOS",3),
+			new Array("MATA PLATANO",4),
+			new Array("IPIS",5),
+			new Array("RANCHO REDONDO",6),
+			new Array("PURRAL",7)
+		),
+		new Array(
+			new Array("SANTA ANA",1),
+			new Array("SALITRAL",2),
+			new Array("POZOS",3),
+			new Array("URUCA",4),
+			new Array("PIEDADES",5),
+			new Array("BRASIL",6)
+		),
+		new Array(
+			new Array("ALAJUELITA",1),
+			new Array("SAN JOSECITO",2),
+			new Array("SAN ANTONIO",3),
+			new Array("CONCEPCIÓN",4),
+			new Array("SAN FELIPE",5)
+		),
+		new Array(
+			new Array("SAN ISIDRO",1),
+			new Array("SAN RAFAEL",2),
+			new Array("DULCE NOMBRE DE JESUS",3),
+			new Array("PATALILLO",4),
+			new Array("CASCAJAL",5)
+		),
+		new Array(
+			new Array("SAN IGNACIO",1),
+			new Array("GUAITIL",2),
+			new Array("PALMICHAL",3),
+			new Array("CANGREJAL",4),
+			new Array("SABANILLAS",5)
+		),
+		new Array(
+			new Array("SAN JUAN",1),
+			new Array("CINCO ESQUINAS",2),
+			new Array("ANSELMO LLORENTE",3),
+			new Array("LEÓN XIII",4)
+		),
+		new Array(
+			new Array("SAN VICENTE",1),
+			new Array("SAN JERONIMO",2),
+			new Array("TRINIDAD",3)
+		),
+		new Array(
+			new Array("SAN PEDRO",1),
+			new Array("SABANILLA",2),
+			new Array("MERCEDES",3),
+			new Array("SAN RAFAEL",4)
+		),
+		new Array(
+			new Array("SAN PABLO",1),
+			new Array("SAN PEDRO",2),
+			new Array("SAN JUAN DE MATA",3),
+			new Array("SAN LUIS",4)
+		),
+		new Array(
+			new Array("SANTA MARIA",1),
+			new Array("JARDIN",2),
+			new Array("COPEY",3)
+		),
+		new Array(
+			new Array("CURRIDABAT",1),
+			new Array("GRANADILLA",2),
+			new Array("SANCHEZ",3),
+			new Array("TIRRASES",4)
+		),
+		new Array(
+			new Array("SAN ISIDRO DE EL GENERAL",1),
+			new Array("GENERAL",2),
+			new Array("DANIEL FLORES",3),
+			new Array("RIVAS",4),
+			new Array("SAN PEDRO",5),
+			new Array("PLATANARES",6),
+			new Array("PEJIBAYE",7),
+			new Array("CAJON",8),
+			new Array("BARU",9),
+			new Array("RIO NUEVO",10),
+			new Array("EL PÁRAMO",11)
+		),
+		new Array(
+			new Array("SAN PABLO",1),
+			new Array("SAN ANDRES",2),
+			new Array("LLANO BONITO",3),
+			new Array("SAN ISIDRO",4),
+			new Array("SANTA CRUZ",5),
+			new Array("SAN ANTONIO",6)
+		)
+	),
+	new Array(
+		new Array(
+			new Array("ALAJUELA",1),
+			new Array("BARRIO SAN JOSE",2),
+			new Array("CARRIZAL",3),
+			new Array("SAN ANTONIO",4),
+			new Array("GUACIMA",5),
+			new Array("SAN ISIDRO",6),
+			new Array("SABANILLA",7),
+			new Array("SAN RAFAEL",8),
+			new Array("RIO SEGUNDO",9),
+			new Array("DESAMPARADOS",10),
+			new Array("TURRUCARES",11),
+			new Array("TAMBOR",12),
+			new Array("GARITA",13),
+			new Array("SARAPIQUI",14)
+		),
+		new Array(
+			new Array("SAN RAMON",1),
+			new Array("SANTIAGO",2),
+			new Array("SAN JUAN",3),
+			new Array("PIEDADES NORTE",4),
+			new Array("PIEDADES SUR",5),
+			new Array("SAN RAFAEL",6),
+			new Array("SAN ISIDRO",7),
+			new Array("ANGELES",8),
+			new Array("ALFARO",9),
+			new Array("VOLIO",10),
+			new Array("CONCEPCION",11),
+			new Array("ZAPOTAL",12),
+			new Array("PEÑAS BLANCAS",13)
+		),
+		new Array(
+			new Array("GRECIA",1),
+			new Array("SAN ISIDRO",2),
+			new Array("SAN JOSE",3),
+			new Array("SAN ROQUE",4),
+			new Array("TACARES",5),
+			new Array("RIO CUARTO",6),
+			new Array("PUENTE DE PIEDRA",7),
+			new Array("BOLIVAR",8)
+		),
+		new Array(
+			new Array("SAN MATEO",1),
+			new Array("DESMONTE",2),
+			new Array("JESUS MARIA",3)
+		),
+		new Array(
+			new Array("ATENAS",1),
+			new Array("JESUS",2),
+			new Array("MERCEDES",3),
+			new Array("SAN ISIDRO",4),
+			new Array("CONCEPCION",5),
+			new Array("SAN JOSE",6),
+			new Array("SANTA EULALIA",7)
+		),
+		new Array(
+			new Array("NARANJO",1),
+			new Array("SAN MIGUEL",2),
+			new Array("SAN JOSE",3),
+			new Array("CIRRI SUR",4),
+			new Array("SAN JERONIMO",5),
+			new Array("SAN JUAN",6),
+			new Array("ROSARIO",7)
+		),
+		new Array(
+			new Array("PALMARES",1),
+			new Array("ZARAGOZA",2),
+			new Array("BUENOS AIRES",3),
+			new Array("SANTIAGO",4),
+			new Array("CANDELARIA",5),
+			new Array("ESQUIPULAS",6),
+			new Array("GRANJA",7)
+		),
+		new Array(
+			new Array("SAN PEDRO",1),
+			new Array("SAN JUAN",2),
+			new Array("SAN RAFAEL",3),
+			new Array("CARRILLOS",4),
+			new Array("SABANA REDONDA",5)
+		),
+		new Array(
+			new Array("OROTINA",1),
+			new Array("MASTATE",2),
+			new Array("HACIENDA VIEJA",3),
+			new Array("COYOLAR",4),
+			new Array("CEIBA",5)
+		),
+		new Array(
+			new Array("CIUDAD QUESADA",1),
+			new Array("FLORENCIA",2),
+			new Array("BUENAVISTA",3),
+			new Array("AGUAS ZARCAS",4),
+			new Array("VENECIA",5),
+			new Array("PITAL",6),
+			new Array("FORTUNA",7),
+			new Array("TIGRA",8),
+			new Array("PALMERA",9),
+			new Array("VENADO",10),
+			new Array("CUTRIS",11),
+			new Array("MONTERREY",12),
+			new Array("SANTA ROSA POCOSOL",13)
+		),
+		new Array(
+			new Array("ZARCERO",1),
+			new Array("LAGUNA",2),
+			new Array("TAPEZCO",3),
+			new Array("GUADALUPE",4),
+			new Array("PALMIRA",5),
+			new Array("ZAPOTE",6)
+		),
+		new Array(
+			new Array("SARCHI NORTE",1),
+			new Array("SARCHI SUR",2),
+			new Array("TORO AMARILLO",3),
+			new Array("SAN PEDRO",4),
+			new Array("RODRIGUEZ",5)
+		),
+		new Array(
+			new Array("UPALA",1),
+			new Array("AGUAS CLARAS",2),
+			new Array("SAN JOSE O PIZOTE",3),
+			new Array("BIJAGUA",4),
+			new Array("DELICIAS",5),
+			new Array("DOS RIOS",6),
+			new Array("YOLILLAL",7)
+		),
+		new Array(
+			new Array("LOS CHILES",1),
+			new Array("CAÑO NEGRO",2),
+			new Array("EL AMPARO",3),
+			new Array("SAN JORGE",4)
+		),
+		new Array(
+			new Array("SAN RAFAEL",1),
+			new Array("BUENAVISTA",2),
+			new Array("COTE",3)
+		)
+	),
+	new Array(
+		new Array(
+			new Array("ORIENTAL",1),
+			new Array("OCCIDENTAL",2),
+			new Array("CARMEN",3),
+			new Array("SAN NICOLAS",4),
+			new Array("AGUACALIENTE",5),
+			new Array("GUADALUPE",6),
+			new Array("CORRALILLO",7),
+			new Array("TIERRA BLANCA",8),
+			new Array("DULCE NOMBRE",9),
+			new Array("LLANO GRANDE",10),
+			new Array("QUEBRADILLA",11)
+		),
+		new Array(
+			new Array("PARAISO",1),
+			new Array("SANTIAGO",2),
+			new Array("OROSI",3),
+			new Array("CACHI",4)
+		),
+		new Array(
+			new Array("TRES RIOS",1),
+			new Array("SAN DIEGO",2),
+			new Array("SAN JUAN",3),
+			new Array("SAN RAFAEL",4),
+			new Array("CONCEPCION",5),
+			new Array("DULCE NOMBRE",6),
+			new Array("SAN RAMON",7),
+			new Array("RIO AZUL",8)
+		),
+		new Array(
+			new Array("JUAN VIÑAS",1),
+			new Array("TUCURRIQUE",2),
+			new Array("PEJIBAYE",3)
+		),
+		new Array(
+			new Array("TURRIALBA",1),
+			new Array("LA SUIZA",2),
+			new Array("PERALTA",3),
+			new Array("SANTA CRUZ",4),
+			new Array("SANTA TERESITA",5),
+			new Array("PAVONES",6),
+			new Array("TUIS",7),
+			new Array("TAYUTIC",8),
+			new Array("SANTA ROSA",9),
+			new Array("TRES EQUIS",0)
+		),
+		new Array(
+			new Array("PACAYAS",1),
+			new Array("CERVANTES",2),
+			new Array("CAPELLADES",3)
+		),
+		new Array(
+			new Array("SAN RAFAEL",1),
+			new Array("COT",2),
+			new Array("POTRERO CERRADO",3),
+			new Array("CIPRESES",4),
+			new Array("SANTA ROSA",5)
+		),
+		new Array(
+			new Array("TEJAR",1),
+			new Array("SAN ISIDRO",2),
+			new Array("TOBOSI",3),
+			new Array("PATIO DE AGUA",4)
+		)
+	),
+	new Array(
+		new Array(
+			new Array("HEREDIA",1),
+			new Array("MERCEDES",2),
+			new Array("SAN FRANCISCO",3),
+			new Array("ULLOA - BARREAL",4),
+			new Array("VARA BLANCA",5)
+		),
+		new Array(
+			new Array("BARVA",1),
+			new Array("SAN PEDRO",2),
+			new Array("SAN PABLO",3),
+			new Array("SAN ROQUE",4),
+			new Array("SANTA LUCIA",5),
+			new Array("SAN JOSE LA MONTANA",6)
+		),
+		new Array(
+			new Array("SANTO DOMINGO",1),
+			new Array("SAN VICENTE",2),
+			new Array("SAN MIGUEL",3),
+			new Array("PARACITO",4),
+			new Array("SANTO TOMAS",5),
+			new Array("SANTA ROSA",6),
+			new Array("TURES",7),
+			new Array("PARA",8)
+		),
+		new Array(
+			new Array("SANTA BARBARA",1),
+			new Array("SAN PEDRO",2),
+			new Array("SAN JUAN",3),
+			new Array("JESUS",4),
+			new Array("SANTO DOMINGO",5),
+			new Array("PURABA",6)
+		),
+		new Array(
+			new Array("SAN RAFAEL",1),
+			new Array("SAN JOCESITO",2),
+			new Array("SANTIAGO",3),
+			new Array("ANGELES",4),
+			new Array("CONCEPCION",5)
+		),
+		new Array(
+			new Array("SAN ISIDRO",1),
+			new Array("SAN JOSE",2),
+			new Array("CONCEPCION",3)
+		),
+		new Array(
+			new Array("SAN ANTONIO",1),
+			new Array("LA RIBERA",2),
+			new Array("ASUNCION",3)
+		),
+		new Array(
+			new Array("SAN JOAQUIN",1),
+			new Array("BARRANTES",2),
+			new Array("LLORENTE",3)
+		),
+		new Array(
+			new Array("SAN PABLO",1)
+		),
+		new Array(
+			new Array("PUERTO VIEJO",1),
+			new Array("LA VIRGEN",2),
+			new Array("HORQUETAS",3)
+		)
+	),
+	new Array(
+		new Array(
+			new Array("LIBERIA",1),
+			new Array("CAÑAS DULCES",2),
+			new Array("MAYORGA",3),
+			new Array("NACASCOLO",4),
+			new Array("CURUBANDE",5)
+		),
+		new Array(
+			new Array("NICOYA",1),
+			new Array("MANSION",2),
+			new Array("SAN ANTONIO",3),
+			new Array("QUEBRADA HONDA",4),
+			new Array("SAMARA",5),
+			new Array("NOSARA",6),
+			new Array("BELEN DE NOSARITA",7)
+		),
+		new Array(
+			new Array("SANTA CRUZ",1),
+			new Array("BOLSON",2),
+			new Array("VEINTISIETE DE ABRIL",3),
+			new Array("TEMPATE",4),
+			new Array("CARTAGENA",5),
+			new Array("CUAJINIQUIL",6),
+			new Array("DIRIA",7),
+			new Array("CABO VELAS",8),
+			new Array("TAMARINDO",9)
+		),
+		new Array(
+			new Array("BAGACES",1),
+			new Array("FORTUNA",2),
+			new Array("MOGOTE",3),
+			new Array("RIO NARANJO",4)
+		),
+		new Array(
+			new Array("FILADELFIA",1),
+			new Array("PALMIRA",2),
+			new Array("SARDINAL",3),
+			new Array("BELEN",4)
+		),
+		new Array(
+			new Array("CAÑAS",1),
+			new Array("PALMIRA",2),
+			new Array("SAN MIGUEL",3),
+			new Array("BEBEDERO",4),
+			new Array("POROZAL",5)
+		),
+		new Array(
+			new Array("JUNTAS",1),
+			new Array("SIERRA",2),
+			new Array("SAN JUAN",3),
+			new Array("COLORADO",4)
+		),
+		new Array(
+			new Array("TILARAN",1),
+			new Array("QUEBRADA GRANDE",2),
+			new Array("TRONADORA",3),
+			new Array("SANTA ROSA",4),
+			new Array("TIERRAS MORENAS",6),
+			new Array("ARENAL",7)
+		),
+		new Array(
+			new Array("CARMONA",1),
+			new Array("SANTA RITA",2),
+			new Array("ZAPOTAL",3),
+			new Array("SAN PABLO",4),
+			new Array("PORVENIR",5),
+			new Array("BEJUCO",6)
+		),
+		new Array(
+			new Array("LA CRUZ",1),
+			new Array("SANTA CECILIA",2),
+			new Array("GARITA",3),
+			new Array("SANTA ELENA",4)
+		),
+		new Array(
+			new Array("HOJANCHA",1)
+		)
+	),
+	new Array(
+		new Array(
+			new Array("PUNTARENAS",1),
+			new Array("PITAHAYA",2),
+			new Array("CHOMES",3),
+			new Array("LEPANTO",4),
+			new Array("PAQUERA",5),
+			new Array("MANZANILLO",6),
+			new Array("GUACIMAL",7),
+			new Array("BARRANCA",8),
+			new Array("MONTE VERDE",9),
+			new Array("ISLA DEL COCO",10),
+			new Array("COBANO",11),
+			new Array("CHACARITA",13),
+			new Array("JICARAL",14)
+		),
+		new Array(
+			new Array("ESPIRITU SANTO",1),
+			new Array("SAN JUAN GRANDE",2),
+			new Array("MACACONA",3),
+			new Array("SAN RAFAEL",4),
+			new Array("SAN JERONIMO",5)
+		),
+		new Array(
+			new Array("BUENOS AIRES",1),
+			new Array("VOLCAN",2),
+			new Array("POTRERO GRANDE",3),
+			new Array("BORUCA",4),
+			new Array("PILAS",5),
+			new Array("COLINAS",6),
+			new Array("CHANGUENA",7),
+			new Array("BIOLLEY",8),
+			new Array("BRUNKA",9)
+		),
+		new Array(
+			new Array("MIRAMAR",1),
+			new Array("UNION",2),
+			new Array("SAN ISIDRO",3)
+		),
+		new Array(
+			new Array("PUERTO CORTES",1),
+			new Array("PALMAR NORTE",2),
+			new Array("SIERPE",3),
+			new Array("BAHIA BALLENA",4),
+			new Array("PIEDRAS BLANCAS",5)
+		),
+		new Array(
+			new Array("QUEPOS",1),
+			new Array("SAVEGRE",2),
+			new Array("NARANJITO",3)
+		),
+		new Array(
+			new Array("GOLFITO",1),
+			new Array("PUERTO JIMENEZ",2),
+			new Array("GUAYCARA",3),
+			new Array("PAVON",4)
+		),
+		new Array(
+			new Array("SAN VITO",1),
+			new Array("SABALITO",2),
+			new Array("AGUA BUENA",3),
+			new Array("LIMONCITO",4),
+			new Array("PITTIER",5)
+		),
+		new Array(
+			new Array("PARRITA",1)
+		),
+		new Array(
+			new Array("CORREDOR",1),
+			new Array("LA CUESTA",2),
+			new Array("CANOAS",3),
+			new Array("LAUREL",4)
+		),
+		new Array(
+			new Array("JACO",1),
+			new Array("TARCOLES",2)
+		)
+	),
+	new Array(
+		new Array(
+			new Array("LIMON",1),
+			new Array("VALLE LA ESTRELLA",2),
+			new Array("RIO BLANCO",3),
+			new Array("MATAMA",4),
+			new Array("BARRA TORTUGUERO",5)
+		),
+		new Array(
+			new Array("GUAPILES",1),
+			new Array("JIMENEZ",2),
+			new Array("RITA",3),
+			new Array("ROXANA",4),
+			new Array("CARIARI",5),
+			new Array("COLORADO",6)
+		),
+		new Array(
+			new Array("SIQUIRRES",1),
+			new Array("PACUARITO",2),
+			new Array("FLORIDA",3),
+			new Array("GERMANIA",4),
+			new Array("CAIRO",5),
+			new Array("ALEGRIA",6)
+		),
+		new Array(
+			new Array("BRATSI",1),
+			new Array("SIXAOLA",2),
+			new Array("CAHUITA",3)
+		),
+		new Array(
+			new Array("MATINA",1),
+			new Array("BATAN",2),
+			new Array("CARRANDI",3)
+		),
+		new Array(
+			new Array("GUACIMO",1),
+			new Array("MERCEDES",2),
+			new Array("POCORA",3),
+			new Array("RIO JIMENEZ",4),
+			new Array("DUACARI",5)
+		)
+	)
+	);
+	
+	
+	function fillSelectFromArray(selectCtrl, itemArray, goodPrompt, badPrompt, defaultItem) {
+	var i, j;
+	var prompt;
+	// empty existing items
+	for (i = selectCtrl.options.length; i >= 0; i--) {
+	selectCtrl.options[i] = null; 
+	}
+	prompt = (itemArray != null) ? goodPrompt : badPrompt;
+	if (prompt == null) {
+	j = 0;
+	}
+	else {
+	selectCtrl.options[0] = new Option(prompt);
+	j = 1;
+	}
+	if (itemArray != null) {
+	// add new items
+	for (i = 0; i < itemArray.length; i++) {
+	selectCtrl.options[j] = new Option(itemArray[i][0]);
+	if (itemArray[i][1] != null) {
+	selectCtrl.options[j].value = itemArray[i][1];// + "-" + itemArray[i][0]; 
+	}
+	j++;
+	}
+	// select first item (prompt) for sub list
+	selectCtrl.options[0].selected = true;
+	   }
+	}
 
