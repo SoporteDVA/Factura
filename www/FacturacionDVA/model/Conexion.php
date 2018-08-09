f79d8d9f06f4ba77ae8d8e13965c5c195ae7ac83
DVA <soporte@corpdva.com>
2018-08-09 13:05:43 -0600

    9988

--- www/FacturacionDVA/model/Conexion.php
+++ www/FacturacionDVA/model/Conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conexion = new mysqli("localhost", "dbsolventas", "dbsolventas", "dbsolventas");
+	$conexion = new mysqli("167.114.58.131", "dbsolventas", "dbsolventas", "dbsolventas");
 	
 	if (mysqli_connect_errno()) {
 	    printf("Connect failed: %s\n", mysqli_connect_error());
